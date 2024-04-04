e00db6767ad39e46aa640e7a727bf51b16c0b3db
rockerofasy <ankitsingh@heyhomie.me>
2024-04-05 01:32:26 +0530

    backend server ka taiyar bhai

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { registerUser } = require("../Controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  findUser,
+  findAllUsers,
+} = require("../Controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/find/:userId", findUser);
+router.get("/", findAllUsers);
 
 module.exports = router;
